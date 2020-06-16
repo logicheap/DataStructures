19be22eb7f1e8767e73acb9eedcda4ece28879db
logicheap <60844331+logicheap@users.noreply.github.com>
2020-06-16 13:52:35 +0530

    Update LinkedListDeletion.py

--- LinkedListDeletion.py
+++ LinkedListDeletion.py
@@ -1,81 +1,150 @@
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def printList(self):
-        temp = self.head
-        print("Printing linked list")
-        while temp:
-            print(temp.data)
-            temp = temp.next
-
-    def insertAtBeginning(self, data):
-        temp = Node(data)
-        temp.next = self.head
-        self.head = temp
-
-    def insertAfter(self, prev_node, data):
-        if prev_node is None:
-            print("Please provide a valid reference")
-            return
-        temp = Node(data)
-        temp.next = prev_node.next
-        prev_node.next = temp
-
-    def append(self, data):
-        temp = Node(data)
-        last_node = self.head
-        if self.head is None:
-            self.head = temp
-        while last_node.next :
-            last_node = last_node.next
-        last_node.next = temp
-
-    def delete(self, data):
-        prev_node = None
-        cur_node = self.head
-        if cur_node is not None:
-            if cur_node.data == data:
-                self.head = cur_node.next
-                cur_node = None
-                return
-        while cur_node:
-            if cur_node.data == data:
-                prev_node.next = cur_node.next
-                cur_node = None
-                return
-            prev_node = cur_node
-            cur_node = cur_node.next
+#This import is of the class Queue that we created in Queue chapter(https://github.com/logicheap/DataStructures/blob/master/Queue.py)
+from queue import Queue
 
 class Node:
     def __init__(self, data):
+        self.left = None
+        self.right = None
         self.data = data
-        self.next = None
 
-ll = LinkedList()
+class BinaryTree:
+    def __init__(self):
+        self.root = None
 
-firstNode = Node(10)
-secondNode = Node(20)
-thirdNode = Node(30)
-fourthNode = Node(40)
+    def inorder_traversal(self, root):
+        if root==None:
+            return
+        else:
+            self.inorder_traversal(root.left)
+            print(root.data)
+            self.inorder_traversal(root.right)
 
-ll.head = firstNode
-firstNode.next = secondNode
-secondNode.next = thirdNode
-thirdNode.next = fourthNode
+    def preorder_traversal(self, root):
+        if root == None:
+            return
+        else:
+            print(root.data)
+            self.preorder_traversal(root.left)
+            self.preorder_traversal(root.right)
 
-ll.printList()
+    def postorder_traversal(self, root):
+        if root == None:
+            return
+        else:
+            self.postorder_traversal(root.left)
+            self.postorder_traversal(root.right)
+            print(root.data)
 
-ll.insertAtBeginning(5)
-ll.insertAfter(ll.head.next.next.next, 5)
-ll.append(5)
-ll.printList()
-ll.delete(5)
-ll.printList()
-ll.delete(5)
-ll.printList()
-ll.delete(5)
-ll.printList()
+    def levelorder_traversal(self, root):
+        Q = Queue()
+        if root != None:
+            Q.enqueue(root)
+
+        while(not Q.is_empty()):
+            temp = Q.dequeue()
+            print(temp.data)
+            if temp.left is not None:
+                Q.enqueue(temp.left)
+            if temp.right is not None:
+                Q.enqueue(temp.right)
+
+    def insert(self, value):
+        Q = Queue()
+        if self.root != None:
+            Q.enqueue(self.root)
+        else:
+            self.root = Node(value)
+
+        while(len(Q) > 0):
+            temp = Q.dequeue()
+
+            if temp.left is not None:
+                Q.enqueue(temp.left)
+            else:
+                temp.left = Node(value)
+                break
+
+            if temp.right is not None:
+                Q.enqueue(temp.right)
+            else:
+                temp.right = Node(value)
+                break
+
+    def delete_last_node(self, delete_node):
+        Q = Queue()
+        if self.root != None:
+            if(self.root == delete_node):
+                self.root = None
+                return
+            Q.enqueue(self.root)
+
+        while (not Q.is_empty()):
+            temp = Q.dequeue()
+            if temp.left is not None:
+                if temp.left == delete_node:
+                    temp.left = None
+                    break
+                else:
+                    Q.enqueue(temp.left)
+            if temp.right is not None:
+                if temp.right == delete_node:
+                    temp.right = None
+                    break
+                else:
+                    Q.enqueue(temp.right)
+
+
+    def delete(self, value):
+        Q = Queue()
+        if self.root != None:
+            Q.enqueue(self.root)
+        while (not Q.is_empty()):
+            temp = Q.dequeue()
+            if temp.data == value:
+                key_node = temp
+            if temp.left is not None:
+                Q.enqueue(temp.left)
+            if temp.right is not None:
+                Q.enqueue(temp.right)
+
+        if key_node:
+            key_node.data = temp.data
+            self.delete_last_node(temp)
+
+
+T = BinaryTree()
+
+T.root = Node(10)
+T.root.left = Node(20)
+T.root.right = Node(30)
+T.root.left.left = Node(40)
+T.root.left.right = Node(50)
+T.root.right.right = Node(60)
+print("Inorder traversal of tree")
+T.inorder_traversal(T.root)
+print("Preorder traversal of tree")
+T.preorder_traversal(T.root)
+print("Postorder traversal of tree")
+T.postorder_traversal(T.root)
+print("Levelorder traversal of tree")
+T.levelorder_traversal(T.root)
+value = input("Provide a value for the insertion")
+T.insert(value)
+print("Levelorder traversal of tree after insertion")
+T.levelorder_traversal(T.root)
+T.levelorder_traversal(T.root)
+print("deleting")
+T.delete(10)
+T.levelorder_traversal(T.root)
+
+T2 = BinaryTree()
+T2.root = Node(10)
+T2.levelorder_traversal(T2.root)
+print("deleting")
+T2.delete(10)
+T2.levelorder_traversal(T2.root)
+
+
+
+
 
